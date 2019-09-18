187fff603dc67d93663e0d7cecee270a29d3c928
Puck-The-Bard <zdmcguire1@buffs.wtamu.edu>
2019-09-17 22:43:48 -0500

    Initial commit

---
+++ HW 2/Program.cs
@@ -0,0 +1,19 @@
+﻿/*
+Zach McGuire
+Assignment 2
+9/17/2019
+
+*/
+
+using System;
+
+namespace HW_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+}
